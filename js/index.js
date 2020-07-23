d35124fa1eecee3c6f957effeb5697d68830c8ae
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-07-23 14:22:21 -0500

    console log data

--- js/index.js
+++ js/index.js
@@ -5,3 +5,14 @@ $(".test").click(function () {
     $(this).css("color", "coral");
 });
 
+const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://type.fit/api/quotes",
+    "method": "GET"
+}
+
+$.ajax(settings).done(function (response) {
+    const data = JSON.parse(response);
+    console.log(data);
+});
